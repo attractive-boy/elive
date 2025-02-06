18e7c9995425c60034a591ab1ef63d0ee11d2c5e
李成 <licheng@lichengdeMacBook-Air.local>
2025-02-06 23:34:39 +0800

    1

---
+++ pages/index/index.vue
@@ -0,0 +1,115 @@
+<template>
+	<view class="content">
+
+		<view class="banner-container">
+			<u-image src="/static/banner.jpg" mode="aspectFit" width="1000rpx" height="400rpx"></u-image>
+		</view>
+		<view class="content-container">
+			<view class="content-user-login" @click="tome">
+				<u-avatar :src="avatar_url" shape="circle" size="30"></u-avatar>
+				<view style="width: 30rpx;"></view>
+				<u-text :text="user_nick_name"></u-text>
+			</view>
+			<view class="content-user-control">
+				<view style="width: 60vw;">
+					<u-tabs :list="live_type" @click="change_live_type" :itemStyle="{ padding: '15rpx' }"></u-tabs>
+				</view>
+				<u-search placeholder="搜一下" v-model="keyword" :showAction="false"></u-search>
+			</view>
+
+
+		</view>
+	</view>
+
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			avatar_url: '',
+			user_nick_name: '立即登录',
+			live_type: [
+				{
+					name: '最近',
+					value: 0
+				},
+				{
+					name: '直播中',
+					value: 1
+				},
+				{
+					name: '未开始',
+					value: 2
+				},
+				{
+					name: '已结束',
+					value: 3
+				}
+			],
+			current_live_type: 0,
+			keyword: ''
+
+		}
+	},
+	onLoad() {
+
+	},
+	methods: {
+		change_live_type(val) {
+			this.current_live_type = val;
+		},
+		tome() {
+			uni.navigateTo({
+				url: '/pages/me/me'
+			})
+		}
+	}
+}
+</script>
+
+<style>
+.banner-container {
+	display: flex;
+	justify-content: center;
+	align-items: flex-start;
+	width: 100%;
+	height: 400rpx;
+	background: linear-gradient(180deg, #0d90fe, #2b4dff 35%, #2b4dff);
+	transform: translateY(-100rpx);
+}
+
+.content-container {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
+	width: 100%;
+	height: calc(100vh - 300rpx);
+	transform: translateY(-200rpx);
+	background-color: white;
+	border-radius: 30rpx;
+}
+
+.content-user-login {
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	/* 间隔: 20rpx; */
+	align-items: center;
+	width: 100%;
+	height: 100rpx;
+	padding: 30rpx;
+	box-sizing: border-box;
+}
+
+.content-user-control {
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	align-items: center;
+	height: 100rpx;
+	padding: 30rpx;
+	box-sizing: border-box;
+}
+</style>
---
+++ pages/me/me.vue
@@ -0,0 +1,82 @@
+<template>
+	<view>
+		<view
+			style="width: 100%; height: 40vh; background-image: url('/static/me.png'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+		</view>
+		<view style="transform: translateY(-400rpx);">
+			<view style="display: flex; flex-direction: row;" @click="loginshow = true">
+				<view style="width: 30rpx;"></view>
+				<u-avatar :size="50" :src="user_avatar" style="margin: 0 30rpx"></u-avatar>
+				<view style="width: 30rpx;"></view>
+				<u-text :text="user_name" size="15"></u-text>
+
+			</view>
+			<view
+				style="background-color: white;border-radius: 20rpx;width: calc(100% - 60rpx); margin-left: 30rpx; margin-top: 30rpx; box-shadow: 0 0 10rpx rgba(0, 0, 0, 0.1);">
+				<view style="padding:0 20rpx;">
+					<u-cell-group>
+						<u-cell title="文件盘" value="0.00G/100G" :isLink="true"></u-cell>
+						<u-cell title="我的订单" :is-link="true"></u-cell>
+						<u-cell title="自动预约" :is-link="true" value="未开启"></u-cell>
+						<u-cell title="数据报告" :is-link="true"></u-cell>
+						<u-cell title="手机号" :is-link="true"></u-cell>
+						<u-cell title="实名认证" :is-link="true" value="未认证,请先绑定手机号"></u-cell>
+						<u-cell title="账号注销" :is-link="true" value=""></u-cell>
+						<u-cell title="协议和说明" :is-link="true" value=""></u-cell>
+						<u-cell title="鹅直播APP" :is-link="true" value=""></u-cell>
+					</u-cell-group>
+				</view>
+			</view>
+		</view>
+		<u-popup :show="loginshow" @close="closelogin" @open="openlogin">
+			<view style="margin: 30rpx;">
+				<u-text text="获取用户信息"></u-text>
+				<u-text text="点击头像或昵称可进行修改" style="color: gray;" size="10"></u-text>
+				<view style="display: flex; flex-direction: column; margin-top: 30rpx; align-items: center; width: 100%;">
+					<button open-type="chooseAvatar" @chooseavatar="bindchooseavatar" style="margin-bottom: 20rpx; border: none; background: none; box-shadow: none; padding: 0; border-radius: 50%;">
+						<u-avatar :size="50" :src="login_user.user_avatar"   ></u-avatar>
+					</button>
+					<view style="display: flex; flex-direction: row; margin-bottom: 20rpx;">
+						<u-text text="昵称"></u-text>
+						<u-input v-model="login_user.user_name" placeholder="请输入昵称"></u-input>
+					</view>
+				</view>
+				<u-button type="primary" @click="login">确定</u-button>
+			</view>
+		</u-popup>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				user_avatar: '',
+				user_name: '未登录',
+				loginshow: false,
+				login_user: {
+					user_name: '',
+					user_avatar: '',
+				}
+
+			}
+		},
+		onLoad() {
+			this.user_avatar = uni.getStorageSync('user_avatar')
+			this.user_name = uni.getStorageSync('user_name') || '未登录'
+		},
+		methods: {
+			closelogin() {
+				this.loginshow = false
+			},
+			openlogin() {
+				this.loginshow = true
+			},
+			bindchooseavatar(event){
+				this.login_user.user_avatar = event.detail.avatarUrl
+			}
+		}
+	}
+</script>
+
+<style></style>
